b327a9e962bd6d6b99a4211d8e819b3c96a19736
Uddinrazi <uddinrazi88@gmail.com>
2023-11-03 14:51:36 +0530

    Typescript showdown

--- dist/routes/todos.js
+++ dist/routes/todos.js
@@ -8,23 +8,28 @@ router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 router.post('/todo', (req, res) => {
+    const body = req.body;
     const newTodo = {
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text
     };
     todos.push(newTodo);
+    res.status(201).json({ message: 'Added todo', todo: newTodo, todos: todos });
 });
 router.put('/todo/:todoId', (req, res) => {
-    const tId = req.params.todoId;
+    const params = req.params;
+    const tId = params.todoId;
+    const body = req.body;
     const todoIndx = todos.findIndex((todoItem) => todoItem.id === tId);
     if (todoIndx >= 0) {
-        todos[todoIndx] = { id: todos[todoIndx].id, text: req.body.text };
+        todos[todoIndx] = { id: todos[todoIndx].id, text: body.text };
         return res.status(200).json({ message: 'Updated todo', todos: todos });
     }
     res.status(404).json({ message: 'todos not fount for this id ' });
 });
 router.delete('/todo/:todoId', (req, res, next) => {
-    todos = todos.filter((todoItem) => todoItem.id !== req.params.todoId);
-    res.status(200).json({ message: 'deleted todos', todos: todos });
+    const params = req.params;
+    todos = todos.filter((todoItem) => todoItem.id !== params.todoId);
+    return res.status(200).json({ message: 'deleted todos', todos: todos });
 });
 exports.default = router;
--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -6,6 +6,8 @@ import {Todo} from '../models/todo'
 
 const router = Router();
 
+type RequestBody = {text: string}
+type RequestParams = {todoId : string}
 
 let todos: Todo[] = []
 router.get('/', (req, res, next) => {
@@ -13,18 +15,22 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/todo', (req, res) => {
+    const body = req.body as RequestBody
     const newTodo: Todo = {
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text
     };
     todos.push(newTodo)
+     res.status(201).json({message: 'Added todo', todo: newTodo, todos:todos})
 })
 
 router.put('/todo/:todoId', (req, res) => {
-    const tId = req.params.todoId;
+    const params = req.params as RequestParams
+    const tId = params.todoId;
+    const body = req.body as RequestBody
     const todoIndx = todos.findIndex((todoItem) => todoItem.id === tId)
     if(todoIndx >= 0){
-        todos[todoIndx] = {id: todos[todoIndx].id, text: req.body.text };
+        todos[todoIndx] = {id: todos[todoIndx].id, text: body.text };
         return res.status(200).json({message: 'Updated todo', todos: todos})
     }
     res.status(404).json({message: 'todos not fount for this id '})
@@ -32,8 +38,9 @@ router.put('/todo/:todoId', (req, res) => {
 
    
 router.delete('/todo/:todoId', (req, res, next) => {
-    todos = todos.filter((todoItem) => todoItem.id !== req.params.todoId)
-    res.status(200).json({message: 'deleted todos', todos:todos})
+    const params = req.params as RequestParams
+    todos = todos.filter((todoItem) => todoItem.id !== params.todoId)
+    return res.status(200).json({message: 'deleted todos', todos:todos})
 })
 
 export default router;
